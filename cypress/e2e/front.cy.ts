85cd26a7d3ba2bdbc28bbc860677df355b59e2d9
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-06-29 18:05:13 +0700

    Update

--- cypress/e2e/front.cy.ts
+++ cypress/e2e/front.cy.ts
@@ -5,8 +5,31 @@ describe("Frontend", () => {
   });
   it("creates todo", () => {
     const url = Cypress.env("FRONTEND_URL");
+    const text = (new Date()).getTime().toString()
     cy.visit(url);
-    cy.get("[data-cy='todo-text']").type("yes");
+    cy.get("[data-cy='todo-text']").type(text);
     cy.get("[data-cy='submit']").click();
+    cy.get("[data-cy='todo-item-wrapper']").contains(text)
   });
+
+  it("deletes todo", ()=>{
+    const url = Cypress.env("FRONTEND_URL")
+
+    const text = (new Date()).getTime().toString()
+    cy.visit(url);
+    cy.get("[data-cy='todo-text']").type(text);
+    cy.get("[data-cy='submit']").click();
+    cy.get("[data-cy='todo-item-wrapper']").contains(text).parent().within(
+
+      ()=> {
+
+
+    cy.get("[data-cy='todo-item-delete']").click();
+      }
+    ) 
+
+
+
+
+  })
 });
